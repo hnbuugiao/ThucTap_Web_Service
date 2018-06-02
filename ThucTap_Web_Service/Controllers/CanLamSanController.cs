1e36946bbdc34cbd2cd4be88ba174295fc3b03bb
hnbuugiao <hnbuugiao@gmail.com>
2018-06-02 22:08:20 +0700

    Update CanLamSanController,CanLamSan Model, CanLamSanProcessor, CanLamSanREpository

---
+++ ThucTap_Web_Service/Controllers/CanLamSanController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ThucTap_Web_Service.Models;
+using ThucTap_Web_Service.Processors;
+using Newtonsoft.Json;
+using System.Globalization;
+using Newtonsoft.Json.Converters;
+
+namespace ThucTap_Web_Service.Controllers
+{
+    public class CanLamSanController : ApiController
+    {
+        [HttpPost]
+        public string ThemCLS([FromBody]CanLamSan canlamsan)
+        {
+            if (canlamsan == null)
+            {
+                return "NULL";
+            }
+            return CanLamSanProcessor.AddCLS(canlamsan);
+        }
+
+        [HttpGet]
+        public string HienThiCLS()
+        {
+            List<CanLamSan> list = new List<CanLamSan>();
+            list = CanLamSanProcessor.ShowAllCLS();
+            var convertedJson = JsonConvert.SerializeObject(list);
+            return convertedJson;
+        }
+
+        [HttpGet]
+        public string HienThiCLS(string id)
+        {
+            List<CanLamSan> list = new List<CanLamSan>();
+            list = CanLamSanProcessor.ShowCLS(id);
+
+            var convertedJson = JsonConvert.SerializeObject(list);
+            return convertedJson;
+        }
+
+        [HttpPut]
+        public bool SuaThongTin(CanLamSan canlamsan)
+        {
+            if (canlamsan == null)
+            {
+                return false;
+            }
+            return CanLamSanProcessor.SuaThongTinCLS(canlamsan);
+        }
+
+
+        [HttpDelete]
+        public bool XoaCLS(string id)
+        {
+            if (id == null)
+            {
+                return false;
+            }
+            return CanLamSanProcessor.XoaCLS(id);
+        }
+    }
+}
---
+++ ThucTap_Web_Service/Models/CanLamSan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThucTap_Web_Service.Models
+{
+    public class CanLamSan
+    {
+        public string macls { get; set; }
+        public string tencls { get; set; }
+        public string dvt { get; set; }
+        public int dongia { get; set; }
+
+        public CanLamSan()
+        {
+
+        }
+
+        public CanLamSan(string macls, string tencls, string dvt, int dongia)
+        {
+            this.macls = macls;
+            this.tencls = tencls;
+            this.dvt = dvt;
+            this.dongia = dongia;
+        }
+    }
+}
